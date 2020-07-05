5fb7747e88aa38490c99103756d1902725a9491b
Igo Pereira Barros <igorestacioceut@gmail.com>
2020-07-05 17:21:36 -0300

    arquivo de configuracao da conexao do mongodb na nuvem

--- settings.py
+++ settings.py
@@ -1,5 +1,11 @@
-
+from pymongo import MongoClient
 
 CONFIG = {
-	'MONGO_ATLAS_URI': 'mongodb+srv://admin-mongodb:54321@clusterprojectmongodb-sztyp.mongodb.net/megahack3?retryWrites=true&w=majority'
+	'MONGO_URI': 'mongodb+srv://admin-mongodb:54321@clusterprojectmongodb-sztyp.mongodb.net/megahack3?retryWrites=true&w=majority',
+	'MONGO_DB': 'megahack3'
 }
+
+def connect():
+	client = MongoClient(CONFIG['MONGO_URI'])
+	db = client[CONFIG['MONGO_DB']]
+	return db
