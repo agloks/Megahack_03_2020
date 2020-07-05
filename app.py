9648920266401c0e718a5c64d187a5ef63708480
Igo Pereira Barros <igorestacioceut@gmail.com>
2020-07-05 17:59:36 -0300

    adaptando o app para subir ao servidor da aws

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from cloudant import Cloudant
-from flask import Flask, request, render_template, jsonify, redirect, url_for, send_from_directory, Response
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import atexit
 import os
-import json
-
-import mongoengine
 
 from src.whataspp import handler_conversation, whastapp_api
 from settings import connect
@@ -22,34 +17,6 @@ app.config['SECRET_KEY'] = 'time21'
 #ONLY FOR DEVELOPMENT
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-db_name = 'mydb'
-client = None
-db = None
-
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print('Found VCAP_SERVICES')
-    if 'cloudantNoSQLDB' in vcap:
-        creds = vcap['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-elif "CLOUDANT_URL" in os.environ:
-    client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
-    db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
-        vcap = json.load(f)
-        print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
 
 # On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
@@ -181,11 +148,6 @@ def pagamento():
 
     return jsonify({'pagamentos': pagamentos})
 
-@atexit.register
-def shutdown():
-    if client:
-        client.disconnect()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
-    #app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
